a0b1f7a06a025338093e1f420750212443d9cfb6
Mark Elsinger <elsingmr@gmail.com>
2021-04-25 12:03:49 -0400

    pageup and pagedown shortcuts

--- project/scenes/ug_mover.gd
+++ project/scenes/ug_mover.gd
@@ -5,7 +5,6 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -21,8 +20,17 @@ func _input(event):
 	if event.is_action("ui_left"):
 		position.x += 12
 	if event.is_action("ui_up"):
-		if position.y + 32 <= 0:
+		if position.y + 12 <= 0:
 			position.y += 12
+		else:
+			position.y = 0
 	if event.is_action("ui_down"):
 		position.y -= 12
+	if event.is_action("ui_page_up"):
+		if position.y + 120 <= 0:
+			position.y += 120
+		else:
+			position.y = 0
+	if event.is_action("ui_page_down"):
+		position.y -= 120
 	get_node("counts/depth").update_text(str(int(-position.y / 600)+1) + " Km", null)
