31a2f9613154eb3e6777e3184d13c978135dfb99
Mark Elsinger <elsingmr@gmail.com>
2021-07-18 20:43:35 -0400

    Make build menu appear immediately

--- project/scenes/buildingButton.gd
+++ project/scenes/buildingButton.gd
@@ -11,16 +11,16 @@ export (PackedScene) var shop_menu_scene
 # store path and lookup instead...
 var build_menu_path = null
 
-# fml
-var time_since_created = 0.0
+# kinda hacky but whatever
+#var time_since_created = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("pressed", self, "_button_pressed")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	time_since_created += delta
+#func _process(delta):
+#	time_since_created += delta
 
 func open_build_menu():
 	var build_menu = build_menu_scene.instance()
@@ -49,19 +49,21 @@ func get_should_open_shop():
 	return !driver.is_enemy and driver.type == "port"
 
 func _button_pressed():
-	if time_since_created > 0.1:
-		if get_should_open_all():
-			var build_menu = get_menu()
-			if build_menu == null or !is_instance_valid(build_menu):
-				open_build_menu()
-		elif get_should_open_shop():
-			var build_menu = get_menu()
-			if build_menu == null or !is_instance_valid(build_menu):
-				open_shop_menu()
-		else:
-			var build_menu = get_menu()
-			if build_menu == null or !is_instance_valid(build_menu):
-				open_tunnel_menu()
+	# for some reason the button is clicked immediately, probably because we are 
+	# adding it on the same frame and the mouse is already down. This is fine, 
+	# but if the menu stops opening, it is probably because this became too slow.
+	if get_should_open_all():
+		var build_menu = get_menu()
+		if build_menu == null or !is_instance_valid(build_menu):
+			open_build_menu()
+	elif get_should_open_shop():
+		var build_menu = get_menu()
+		if build_menu == null or !is_instance_valid(build_menu):
+			open_shop_menu()
+	else:
+		var build_menu = get_menu()
+		if build_menu == null or !is_instance_valid(build_menu):
+			open_tunnel_menu()
 	
 func get_menu():
 	if build_menu_path == null:
